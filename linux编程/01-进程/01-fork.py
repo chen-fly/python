92692e0703c5ef668c1e747e00061a4878668bea
chen-fly <136878750@qq.com>
2017-08-30 16:37:52 +0800

    20170830

---
+++ linux编程/01-进程/01-fork.py
@@ -0,0 +1,4 @@
+import os
+#windows下没有fork
+ret = os.fork()
+print('===1===')
---
+++ linux编程/01-进程/03-fork的返回值.py
@@ -0,0 +1,7 @@
+import os
+import time
+
+print('---进程pid是:%d---'%os.getpid())
+time.sleep(30)
+print('---进程pid是:%d---'%os.getpid())
+time.sleep(30)
